1f0992409d6856827fbd50abc46a5de11c52ed45
nadeshda18 <nadeshda02@hotmail.com>
2023-09-07 16:33:19 +0000

    added brackets, void variables

---
+++ pushpall.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	int value;
+
+	if (scanf(" %d ", &value) != 1)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	*stack = new_node;
+}
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	(void)line_number;
+
+	if (*stack == NULL)
+	{
+		return;
+	}
+	stack_t *current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d ", current->n);
+		current = current->next;
+	}
+	printf("\n");
+}
