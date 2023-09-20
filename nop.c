c4978bd2bc35aabce1deacaa635e4b5089f993a7
nadeshda18 <nadeshda02@hotmail.com>
2023-09-20 15:21:08 +0000

    do nothing

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - does not do anything
+ * @stack: firs node of doubly linked list
+ * @value: line number
+ **/
+void nop(stack_t **stack, unsigned int value)
+{
+	(void)stack;
+	(void)value;
+}
