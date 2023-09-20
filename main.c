e0681fb05b91b76515c1fe6d7864e5f4b672430b
nadeshda18 <nadeshda02@hotmail.com>
2023-09-20 15:04:47 +0000

    error message

--- main.c
+++ main.c
@@ -41,13 +41,13 @@ int open_file(char *filename, stack_t **stack)
 
 	if (!filename)
 	{
-		printf("Error: Can't open file %s\n", filename);
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(filename, "r");
 	if (fd == NULL)
 	{
-		printf("Error: Can't open file %s\n", filename);
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&line, &len, fd) != EOF)
@@ -98,7 +98,7 @@ void parse_command(stack_t **stack, char *op, unsigned int line_number)
 
 	if (strlen(op) != 0 && op[0] != '#')
 	{
-		printf("L%i: unkown instructions %s\n", line_number, op);
+		fprintf(stderr, "L%i: unkown instructions %s\n", line_number, op);
 
 		freedlist(*stack);
 		exit(EXIT_FAILURE);
