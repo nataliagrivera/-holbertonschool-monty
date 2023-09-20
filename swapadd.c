1dd8e6ff74d540507ab847e2b7cf80283b8784e1
nadeshda18 <nadeshda02@hotmail.com>
2023-09-20 15:20:10 +0000

    changed error message

---
+++ swapadd.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+/**
+ * swap - swaps the two tops elements of the stack
+ * @stack: head of doubly linked list
+ * @value: ...
+ *
+ **/
+void swap(stack_t **stack, unsigned int value)
+{
+	stack_t *tmp;
+	(void)value;
+
+	if (!(*stack) || !((*stack)->next))
+	{
+		fprintf(stderr, "L%i: can't swap, stack too short\n", value);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	(*stack)->prev = (*stack)->next;
+	(*stack)->next = tmp->next;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = *stack;
+	tmp->next = *stack;
+	(*stack) = (*stack)->prev;
+	}
+
+
+
+/**
+ * add - adds the two tops elements of the stacks
+ * @stack: head of doubly linked list.
+ * @value: ...
+ *
+ * Return: result
+ **/
+void add(stack_t **stack, unsigned int value)
+{
+	int tmp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%i:can't add, stack too short\n", value);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->n;
+	pop(stack, value);
+	(*stack)->n += tmp;
+}
